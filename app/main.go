9ad04f6cdd085623b4a6d54825a1f848d0b95402
lvxiaoyue <lvxiaoyue@360.cn>
2026-05-02 16:50:29 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -21,9 +22,41 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		// what we actually receive is *1\r\n$4\r\nping\r\n
+		line1, _ := reader.ReadString('\n') // *1\r\n
+		line2, _ := reader.ReadString('\n') // $4\r\n
+		line3, _ := reader.ReadString('\n') // PING\r\n
+
+		fmt.Println(line1, line2, line3)
+
+		if line3 == "PING\r\n" {
+			_, err := conn.Write(encodeSimpleString("PONG"))
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+func encodeSimpleString(s string) []byte {
+	return []byte("+" + s + "\r\n")
 }
